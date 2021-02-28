396ff8fbe1f48fe14292f4f4a44969013683463e
Konstantin Senkevich <konstsen@gmail.com>
2021-02-28 14:06:46 +0300

    Add logging.

--- src/trader.rs
+++ src/trader.rs
@@ -4,34 +4,40 @@ pub struct Trader {
 impl agnostic::market::Trader for Trader {
     fn create_order(
         &self,
-        _order: agnostic::order::Order
+        order: agnostic::order::Order
     ) -> agnostic::market::Future<Result<(), String>> {
-        Box::pin(async {
+        Box::pin(async move {
+            log::debug!("Creating order {:#?}", order);
             Ok(())
         })
     }
 
     fn delete_and_create(
         &self,
-        _id: &str,
-        _new_order: agnostic::order::Order,
+        id: &str,
+        new_order: agnostic::order::Order,
     ) -> agnostic::market::Future<Result<String, String>> {
-        Box::pin(async {
+        let id = id.to_owned();
+        Box::pin(async move {
+            log::debug!("Deleting and creating order {:#?} with id {}", new_order, id);
             Ok("Test".to_owned())
         })
     }
 
-    fn delete_order(&self, _id: &str) -> agnostic::market::Future<Result<(), String>> {
-        Box::pin(async {
+    fn delete_order(&self, id: &str) -> agnostic::market::Future<Result<(), String>> {
+        let id = id.to_owned();
+        Box::pin(async move {
+            log::debug!("Deleting order with id: {:#?}", id);
             Ok(())
         })
     }
 
     fn create_trade_from_order(
         &self,
-        _order: agnostic::order::Order
+        order: agnostic::order::Order
     ) -> agnostic::market::Future<Result<(), String>> {
-        Box::pin(async {
+        Box::pin(async move {
+            log::debug!("Creating trade: {:#?}", order);
             Ok(())
         })
     }
