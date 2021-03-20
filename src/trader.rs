ee1e94a1478dbd2ee10ac6451a17535b5d793dac
Konstantin Senkevich <konstsen@gmail.com>
2021-03-20 11:59:59 +0300

    Remove delete_and_create method from trader trait implementation.

--- src/trader.rs
+++ src/trader.rs
@@ -28,31 +28,6 @@ impl agnostic::market::Trader for Trader {
         })
     }
 
-    fn delete_and_create(
-        &self,
-        id: &str,
-        new_order: Order,
-    ) -> market::Future<Result<Trade, String>> {
-        let id = id.to_owned();
-        Box::pin(async move {
-            log::debug!("Deleting and creating order {:#?} with id {}", new_order, id);
-            match new_order.trading_pair.target {
-                Target::Market => Ok(Trade::Market(TradeResult {
-                    id,
-                    trading_pair: new_order.trading_pair,
-                    price: new_order.price,
-                    amount: new_order.amount,
-                })),
-                Target::Limit => Ok(Trade::Limit(OrderWithId {
-                    id,
-                    trading_pair: new_order.trading_pair,
-                    price: new_order.price,
-                    amount: new_order.amount,
-                })),
-            }
-        })
-    }
-
     fn delete_order(&self, id: &str) -> agnostic::market::Future<Result<(), String>> {
         let id = id.to_owned();
         Box::pin(async move {
