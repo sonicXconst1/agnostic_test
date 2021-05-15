49b00fadbf66640ab1af447a022629da644ce1d5
Konstantin Senkevich <konstsen@gmail.com>
2021-05-15 23:20:40 +0300

    Sniffer tests and default merchant.

--- src/merchant.rs
+++ src/merchant.rs
@@ -1,3 +1,8 @@
+use agnostic::market::{Accountant, Sniffer, Trader};
+use agnostic::trading_pair::Side;
+use std::sync::Arc;
+use crate::sniffer::StockGenerator;
+
 pub struct Merchant {
     accountant: std::sync::Arc<dyn agnostic::market::Accountant>,
     trader: std::sync::Arc<dyn agnostic::market::Trader>,
@@ -5,12 +10,9 @@ pub struct Merchant {
 }
 
 impl Merchant {
-    pub fn with_orders(price: f64, amount: f64) -> Merchant {
-        let sniffer = std::sync::Arc::new(crate::sniffer::Sniffer {
-            orders: vec![crate::sniffer::Order { price, amount}; 3],
-        });
-        let accountant = std::sync::Arc::new(crate::accountant::Accountant::default());
-        let trader = std::sync::Arc::new(crate::trader::Trader::default());
+    pub fn with_sniffer(sniffer: Arc<dyn Sniffer>) -> Merchant {
+        let accountant = Arc::new(crate::accountant::Accountant::default());
+        let trader = Arc::new(crate::trader::Trader::default());
         Merchant {
             sniffer,
             trader,
@@ -18,10 +20,10 @@ impl Merchant {
         }
     }
 
-    pub fn with_custom(
-        accountant: std::sync::Arc<dyn agnostic::market::Accountant>,
-        sniffer: std::sync::Arc<dyn agnostic::market::Sniffer>,
-        trader: std::sync::Arc<dyn agnostic::market::Trader>,
+    pub fn custom(
+        accountant: Arc<dyn Accountant>,
+        sniffer: Arc<dyn Sniffer>,
+        trader: Arc<dyn Trader>,
     ) -> Merchant {
         Merchant {
             accountant,
@@ -33,10 +35,26 @@ impl Merchant {
 
 impl Default for Merchant {
     fn default() -> Self {
+        let base_price = 0.5;
+        let price_step = 0.001;
+        let orders_count = 10;
+        let sell_stock_generator = StockGenerator::new(
+            Side::Sell,
+            base_price + price_step,
+            price_step,
+            orders_count);
+        let buy_stock_generator = StockGenerator::new(
+            Side::Buy,
+            base_price - price_step,
+            price_step,
+            orders_count);
         Merchant {
-            accountant: std::sync::Arc::new(crate::accountant::Accountant::default()),
-            trader: std::sync::Arc::new(crate::trader::Trader::default()),
-            sniffer: std::sync::Arc::new(crate::sniffer::Sniffer::default()),
+            accountant: Arc::new(crate::accountant::Accountant::default()),
+            trader: Arc::new(crate::trader::Trader::default()),
+            sniffer: Arc::new(crate::sniffer::Sniffer::fixed_amount(
+                    sell_stock_generator,
+                    buy_stock_generator,
+                    100f64))
         }
     }
 }
@@ -52,5 +70,22 @@ impl agnostic::merchant::Merchant for Merchant {
 
     fn sniffer(&self) -> std::sync::Arc<dyn agnostic::market::Sniffer> {
         self.sniffer.clone()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use agnostic::merchant::Merchant;
+
+    #[test]
+    fn default_merchant() {
+        let merchant = super::Merchant::default();
+        crate::sniffer::test::test_sniffer(
+            merchant.sniffer().as_ref(),
+            0.501,
+            0.510,
+            0.499,
+            0.490,
+            10);
     }
 }
--- src/sniffer.rs
+++ src/sniffer.rs
@@ -1,30 +1,58 @@
-#[derive(Clone, Debug)]
+use agnostic::trading_pair::{Side, Target};
+
+#[derive(Clone, PartialEq, Debug)]
 pub struct Order {
     pub price: f64,
     pub amount: f64,
 }
 
 pub struct Sniffer {
-    pub orders: Vec<Order>
+    pub sell_orders: Vec<Order>,
+    pub buy_orders: Vec<Order>
 }
 
-impl Default for Sniffer {
-    fn default() -> Sniffer {
+pub struct StockGenerator {
+    base_price: f64,
+    step: f64,
+    count: usize
+}
+
+impl StockGenerator {
+    pub fn new(
+        side: Side,
+        base_price: f64,
+        step: f64,
+        count: usize) -> Self {
+        StockGenerator {
+            base_price,
+            step: match side {
+                Side::Buy => -step,
+                Side::Sell => step,
+            },
+            count,
+        }
+    }
+}
+
+impl StockGenerator {
+    pub fn generate_orders(&self, amount: f64) -> Vec<Order> {
+        (0..self.count)
+            .map(|step_index| {
+                let price = self.base_price + (self.step * (step_index as f64));
+                Order {
+                    price,
+                    amount,
+                }
+            })
+            .collect()
+    }
+}
+
+impl Sniffer {
+    pub fn fixed_amount(sell: StockGenerator, buy: StockGenerator, amount: f64) -> Sniffer {
         Sniffer {
-            orders: vec![
-                Order {
-                    price: 4f64,
-                    amount: 4f64,
-                },
-                Order {
-                    price: 4f64,
-                    amount: 4f64,
-                },
-                Order {
-                    price: 4f64,
-                    amount: 4f64,
-                },
-            ],
+            sell_orders: sell.generate_orders(amount),
+            buy_orders: buy.generate_orders(amount),
         }
     }
 }
@@ -33,9 +61,14 @@ impl agnostic::market::Sniffer for Sniffer {
     fn all_the_best_orders(
         &self,
         trading_pair: agnostic::trading_pair::TradingPair,
-        _count: u32,
+        count: u32,
     ) -> agnostic::market::Future<Result<Vec<agnostic::order::Order>, String>> {
-        let orders = self.orders.clone();
+        let orders = match (trading_pair.target, trading_pair.side) {
+            (Target::Market, Side::Sell) => self.buy_orders.clone(),
+            (Target::Market, Side::Buy) => self.sell_orders.clone(),
+            (Target::Limit, Side::Sell) => self.sell_orders.clone(),
+            (Target::Limit, Side::Buy) => self.buy_orders.clone(),
+        };
         Box::pin(async move {
             Ok(orders.into_iter()
                .map(|order| agnostic::order::Order {
@@ -43,6 +76,7 @@ impl agnostic::market::Sniffer for Sniffer {
                    price: order.price,
                    amount: order.amount,
                })
+               .take(count as usize)
                .collect()
             )
         })
@@ -50,21 +84,106 @@ impl agnostic::market::Sniffer for Sniffer {
 
     fn get_my_orders(
         &self,
-        trading_pair: agnostic::trading_pair::TradingPair,
+        _trading_pair: agnostic::trading_pair::TradingPair,
     ) -> agnostic::market::Future<Result<Vec<agnostic::order::OrderWithId>, String>> {
-        let orders = self.orders.clone();
         Box::pin(async move {
-            Ok(orders
-               .into_iter()
-               .enumerate()
-               .map(|(index, order)| agnostic::order::OrderWithId {
-                   id: index.to_string(),
-                   trading_pair: trading_pair.clone(),
-                   amount: order.amount,
-                   price: order.price,
-                })
-               .collect()
-            )
+            Ok(Vec::new())
         })
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod test {
+    use super::*;
+    use agnostic::trading_pair::{TradingPair, Side, Coins, Target};
+    use agnostic::market::Sniffer;
+    use utils::almost_equal;
+
+    #[test]
+    fn empty_stock_generator() {
+        let stock_generator = StockGenerator::new(Side::Sell, 0f64, 0f64, 0);
+        let orders = stock_generator.generate_orders(100f64);
+        assert_eq!(orders.len(), 0)
+    }
+
+    #[test]
+    fn stock_generator_default() {
+        let base_price = 1f64;
+        let step = 0.1f64;
+        let count = 10;
+        let amount = 100f64;
+        let stock_generator = StockGenerator::new(Side::Sell, base_price, step, count);
+        let orders = stock_generator.generate_orders(amount);
+        assert_eq!(orders.len(), count);
+        let first_price = orders.first().unwrap().price;
+        assert!(almost_equal(first_price, base_price));
+        let last_price = orders.last().unwrap().price;
+        assert!(almost_equal(last_price, 1.9f64)); 
+        let stock_generator = StockGenerator::new(Side::Buy, base_price, step, count);
+        let orders = stock_generator.generate_orders(amount);
+        assert_eq!(orders.len(), count);
+        let first_price = orders.first().unwrap().price;
+        assert!(almost_equal(first_price, base_price));
+        let last_price = orders.last().unwrap().price;
+        assert!(almost_equal(last_price, 0.1f64)); 
+    }
+
+    #[test]
+    fn sniffer() {
+        let base_price = 1f64;
+        let step = 0.1f64;
+        let count = 10;
+        let amount = 100f64;
+        let sell_stock_generator = StockGenerator::new(Side::Sell, base_price, step, count);
+        let buy_stock_generator = StockGenerator::new(Side::Buy, base_price, step, count);
+        let sniffer = super::Sniffer::fixed_amount(sell_stock_generator, buy_stock_generator, amount);
+        test_sniffer(&sniffer, base_price, 1.9f64, base_price, 0.1f64, count)
+    }
+
+    pub(crate) fn test_sniffer(
+        sniffer: &dyn Sniffer,
+        expected_first_sell_price: f64,
+        expected_last_sell_price: f64,
+        expected_first_buy_price: f64,
+        expected_last_buy_price: f64,
+        expected_count: usize
+    ) {
+        test_sniffer_side(
+            sniffer,
+            Side::Sell,
+            expected_first_sell_price, 
+            expected_last_sell_price,
+            expected_count);
+        test_sniffer_side(
+            sniffer,
+            Side::Buy,
+            expected_first_buy_price, 
+            expected_last_buy_price,
+            expected_count);
+    }
+
+    fn test_sniffer_side(
+        sniffer: &dyn Sniffer,
+        side: Side,
+        expected_first_price: f64,
+        expected_last_price: f64,
+        expected_count: usize,
+    ) {
+        let count = (expected_count * 2) as u32;
+        let trading_pair = TradingPair {
+            coins: Coins::TonUsdt,
+            side,
+            target: Target::Limit,
+        };
+        let my_orders = sniffer.get_my_orders(trading_pair.clone());
+        let my_orders = tokio_test::block_on(my_orders).unwrap();
+        assert_eq!(my_orders.len(), 0);
+        let all_the_best_orders = sniffer.all_the_best_orders(trading_pair.clone(), count);
+        let all_the_best_orders = tokio_test::block_on(all_the_best_orders).unwrap();
+        assert_eq!(all_the_best_orders.len(), expected_count);
+        let first_order = all_the_best_orders.first().unwrap();
+        assert!(almost_equal(first_order.price, expected_first_price));
+        let last_order = all_the_best_orders.last().unwrap();
+        assert!(almost_equal(last_order.price, expected_last_price), "Last sell order: {:#?}", last_order);
     }
 }
